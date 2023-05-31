235b546f0c1d630c1ec6b05189f608e93ce44d32
Miguel Angelo John <im.mikel@hotmail.com>
2023-05-31 15:31:18 +0200

    curriculum page finished

--- src/curriculumPage/CurriculumPage.jsx
+++ src/curriculumPage/CurriculumPage.jsx
@@ -1,28 +1,43 @@
 import './CurriculumPage.css'
 import PageLayout from '../pageLayout/PageLayout'
 import { useState } from 'react'
+import DeutschCV from  './../cv/Deutsch-cv.pdf'
+import EnglischCV from'../cv/English-cv.pdf'
+
+
 
 function CurriculumPage() {
-
     const [selectedCv, setSelectedCv] = useState('curriculum-en')
     const [valuebutton, setValueButton] = useState("download")
 
 
 
     const HandlerCv = (event) => {
+
         setSelectedCv(event.target.value)
 
-        if (event.target.value === "lebenslauf") {
-            setValueButton("Herunterladen")
+    if (event.target.value === "lebenslauf") {
+        setValueButton("Herunterladen")
+    } else if (event.target.value === "curricullum-es") {
+         setValueButton("Descargar")
+    } else if (event.target.value === "curriculum-en") {
+         setValueButton("Download")
+    }
+    };
 
-        } if (event.target.value === "curricullum-es") { setValueButton("Descargar") } if(event.target.value==="curriculum-en"){setValueButton("Download")}
+
+    const leftMainCurriculum = (<h1 className="contactme">Download <span className='me'>Me</span></h1>)
+
+
+    const openPDf = () => {
+
+        if (selectedCv === "lebenslauf") { window.open(DeutschCV, 'blank') }
+        if (selectedCv === "curriculum-en") {
+            window.open(EnglischCV, 'blank')
+        }
 
     }
 
-    const leftMainCurriculum=(
-        <h1 className="contactme">Download <span className='me'>Me</span></h1>
-
-    )
     const curricullumSelect = (
         <div className='main-curriculumRight'>
 
@@ -32,13 +47,13 @@ function CurriculumPage() {
                 <option value="curricullum-es">Curriculum(Es) </option>
             </select>
 
-            <button className='curriculum-button'>{valuebutton}</button>
+            <button className='curriculum-button' onClick={openPDf}>{valuebutton}</button>
         </div>)
 
 
     return (
 
-        <PageLayout  left={leftMainCurriculum} right={curricullumSelect}></PageLayout>
+        <PageLayout left={leftMainCurriculum} right={curricullumSelect}></PageLayout>
     )
 }
 
